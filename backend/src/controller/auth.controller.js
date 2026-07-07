c5cfe85e8633064f5fe7b3ae6e8ddab9f1a3e92d
Harshita Bisht <harshitabisht794@gmail.com>
2026-07-07 14:09:35 +0530

    Before upgrading Prisma to v7

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,3 +1,37 @@
-export const RegisterUser=async(req,res)=>{
-console.log("user register");
+import {PrismaClient} from "@prisma/client";
+import bycrypt from "bcryptjs";
+import crypto from "crypto";
+
+const prisma=new PrismaClient();
+
+export const registerUser=(req,res)=>{
+    const {name,email,password}=req.body;
+    if(!name || !email || !password){
+       console.log("all filed are required")
+
+       return res.status(400).json({
+        success:false,
+        message:"All fields are required"
+       })
+    }
+    try {
+       const existingUser=await prisma.user.findUnique({
+            where:{email}
+        })
+
+        if(existingUser){
+        return res.status(400).json({
+        success:false,
+        message:"User alreaddy exist"
+       }) 
+        }
+
+        //hash password
+      const hashPassword=await  bycrypt.hash(password,10);
+     const verificationToken= crypto.randomBytes(32).toString("hex")
+    } catch (error) {
+        
+    }
 }
+
+
